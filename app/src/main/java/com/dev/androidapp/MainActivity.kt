fb8ee61af2c68cb8cb02a557e33f1d30e0dc358a
andreeafrincu <frincu60@gmail.com>
2022-06-11 00:36:18 +0300

    Added animation to splash screen

--- app/src/main/java/com/dev/androidapp/MainActivity.kt
+++ app/src/main/java/com/dev/androidapp/MainActivity.kt
@@ -2,13 +2,20 @@ package com.dev.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        val topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        splashImage.startAnimation(topAnimation)
+        splashText.startAnimation(bottomAnimation)
     }
 }
