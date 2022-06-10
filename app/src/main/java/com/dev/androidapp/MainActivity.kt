e9735fd173394068d7ab42a4c477bab35e97d1cb
andreeafrincu <frincu60@gmail.com>
2022-06-11 02:58:21 +0300

    Added Google sign in with Firebase

--- app/src/main/java/com/dev/androidapp/MainActivity.kt
+++ app/src/main/java/com/dev/androidapp/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.dev.androidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.Animation
+import android.os.Handler
 import android.view.animation.AnimationUtils
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    /** declare variables */
+    private lateinit var myAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +22,20 @@ class MainActivity : AppCompatActivity() {
 
         splashImage.startAnimation(topAnimation)
         splashText.startAnimation(bottomAnimation)
+
+        myAuth = FirebaseAuth.getInstance()
+        val user = myAuth.currentUser
+
+        /** go to sign in activity if user is not logged in */
+        Handler().postDelayed({
+            if (user != null) {
+                startActivity(Intent(this, DashboardActivity::class.java))
+                finish()
+            }
+            else {
+                startActivity(Intent(this, SignInActivity::class.java))
+                finish()
+            }
+        }, 2000)
     }
 }
